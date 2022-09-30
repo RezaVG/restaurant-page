63fc251ce6d97330766d5ce2ce43ff56a389c2de
RezaVG <rezabv.clashofclans@gmail.com>
2022-09-30 23:56:41 +0330

    added tab modules

---
+++ src/about.js
@@ -0,0 +1,6 @@
+export default function about() {
+  const title = document.createElement("h1");
+  title.textContent = "About...";
+
+  return title;
+}
---
+++ src/menu.js
@@ -0,0 +1,6 @@
+export default function menu() {
+  const title = document.createElement("h1");
+  title.textContent = "Menu...";
+
+  return title;
+}
